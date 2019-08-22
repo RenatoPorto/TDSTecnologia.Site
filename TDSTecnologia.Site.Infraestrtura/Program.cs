be1d182e260f5b06fe07bdf21635504e1dbe35ff
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-08-22 19:52:51 -0400

    Iniciando sistema

---
+++ TDSTecnologia.Site.Infraestrtura/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDSTecnologia.Site.Infraestrtura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
