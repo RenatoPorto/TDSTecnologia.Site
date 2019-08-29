60e917b6b6e984d1ed2a4ed194c6e2b995efba39
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-08-29 19:58:54 -0400

    Criando o metado enum

---
+++ TDSTecnologia.Site.Core/Dominio/DomTurno.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Core.Dominio
+{
+    class DomTurno
+    {
+    }
+}
--- TDSTecnologia.Site.Web/Controllers/HomeController.cs
+++ TDSTecnologia.Site.Web/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using TDSTecnologia.Site.Core.Entities;
 using TDSTecnologia.Site.Infrastructure;
@@ -16,6 +20,15 @@ namespace TDSTecnologia.Site.Web.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            List<Curso> cursos = await _context.CursoDao.ToListAsync();
+
+            cursos.ForEach(c =>
+            {
+                if (c.Banner != null)
+                {
+                    c.BannerBase64 = "data:image/png;base64," + Convert.ToBase64String(c.Banner, 0, c.Banner.Length);
+                }
+            });
             return View(await _context.CursoDao.ToListAsync());
         }
 
@@ -29,8 +42,15 @@ namespace TDSTecnologia.Site.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Novo([Bind("Id,Nome,Descricao,QuantidadeAula,DataInicio")] Curso curso)
+        public async Task<IActionResult> Novo([Bind("Id,Nome,Descricao,QuantidadeAula,DataInicio,Turno")] Curso curso, IFormFile arquivo)
         {
+            if (arquivo != null && arquivo.ContentType.ToLower().StartsWith("image/"))
+            {
+                MemoryStream ms = new MemoryStream();
+                await arquivo.OpenReadStream().CopyToAsync(ms);
+                curso.Banner = ms.ToArray();
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Add(curso);
