279768b5a35f181b343d18b9b8a365abe935132e
Renato Rodrigues <renato_rodrigues2@yahoo.com.br>
2019-09-05 19:41:12 -0400

    Repository crie a classe BasicRepository e CursoRespository

---
+++ TDSTecnologia.Site.Infrastructure/Repository/BasicRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Infrastructure
+{
+    public class BasicRepository
+    {
+        protected readonly AppContexto _context;
+
+        public BasicRepository(AppContexto context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ TDSTecnologia.Site.Infrastructure/Repository/CursoRespository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDSTecnologia.Site.Infrastructure.Repository
+{
+    public class CursoRespository : BasicRepository
+    {
+        public CursoRespository(AppContexto context) : base(context)
+        {
+        }
+    }
+}
